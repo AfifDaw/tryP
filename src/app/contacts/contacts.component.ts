453242aea91849abefa524fb2aa32a2e2f31b25d
AfifDaw <61279905+AfifDaw@users.noreply.github.com>
2021-06-10 23:34:17 +0300

    first commit

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -13,8 +13,7 @@ export class ContactsComponent implements OnInit {
   }
 
   developers : any =[
-    {Name: "Afif Daw", ID: 314879370, Country: "ISRAEL", Email: "Afifdaw1@hotmail.com", Img:"https://occ-0-2621-778.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABWBoerl-TmDTocItKd04Wdi_YQWzM5rIFhGZhBPXxXKOqKvzTLq2s-CGMoC-W-jgVElZ0OLSD0U1AQbENsnXXEcn2Z4K.png?r=84f"},
-    {Name: "Soraya Abulhija", ID: 322629874, Country: "USA CALI", Email: "jaboalhija@gmail.com", Img: "https://play-lh.googleusercontent.com/rCYSms-DkQHY3b6Oak-ZuPgRkhM5WOTq8w83d5xcdLF7oNqFIfxeUY2-kRLy5Tdp0WI5=rw"}
+    {Name: "Afif Daw", ID: 0, Country: "Planet Earth", Email: "Afifdaw", Img:"https://occ-0-2621-778.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABWBoerl-TmDTocItKd04Wdi_YQWzM5rIFhGZhBPXxXKOqKvzTLq2s-CGMoC-W-jgVElZ0OLSD0U1AQbENsnXXEcn2Z4K.png?r=84f"},
   ];
 
 }
